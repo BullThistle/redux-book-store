d5ee5e33491f3b8214a90958678aceff2a5660b1
Rafa <rfurry@gmail.com>
2018-04-09 22:01:38 -0700

    Create book reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Present Shock' },
+    { title: 'Dawn of the New Everything' },
+    { title: 'Born a Crime' },
+    { title: 'After On' },
+  ]
+}
