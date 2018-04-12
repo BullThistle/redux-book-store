644c689d861dc700e93f0adb756c8a211e3b33ab
Rafa <rfurry@gmail.com>
2018-04-12 02:12:04 -0700

    Update book detail

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Header } from 'semantic-ui-react';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
   render() {
+    if (!this.props.book) {
+      return <div>Select a book to get started.</div>;
+    }
     return (
-      <div>Book Detail</div>
+      <div>
+        <Header>Details for: </Header>
+        <div>{this.props.book.title}</div>
+      </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  }
+}
+
+export default connect(mapStateToProps)(BookDetail);
